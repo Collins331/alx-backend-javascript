4608e3c92773b91db55ab7eb86e3d863cb484af8
Lincoln <ochiengcollinsoduor2@gmail.com>
2024-04-22 10:40:15 +0000

    update variable declaration

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    var task1 = true;
-    var task3 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
