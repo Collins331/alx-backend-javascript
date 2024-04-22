8b79a703011ba9460f06bf274cf5cff68451c34e
Lincoln <ochiengcollinsoduor2@gmail.com>
2024-04-22 22:06:43 +0000

    iterator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = []
+    const newArray = [];
     for (const item of array) {
         newArray.push(`${appendString}${item}`);
   };
