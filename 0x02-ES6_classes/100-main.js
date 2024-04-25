f12cce1570bc608c13aad18999b2d3358641d99f
Collins <ochiengcollinsoduor2@gmail.com>
2024-04-25 19:36:46 +0300

    update files

---
+++ 0x02-ES6_classes/100-main.js
@@ -0,0 +1,16 @@
+import EVCar from './100-evcar.js';
+
+const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 100-main.js
+EVCar {
+  _brand: 'Tesla',
+  _motor: 'Turbo',
+  _color: 'Red',
+  _range: '250'
+}
---
+++ 0x02-ES6_classes/9-main.js
@@ -0,0 +1,7 @@
+import listOfStudents from "./9-hoisting.js";
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
