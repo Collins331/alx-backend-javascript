5fa728fccdb2b42caa2baf31b32ebbf17315722e
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-01 01:03:47 +0300

    update maps and weakmaps

--- 0x03-ES6_data_manipulation/7-main.js
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -1,4 +1,4 @@
-import hasValuesFromArray from "./7-has_array_values.js";
+import hasValuesFromArray from './7-has_array_values';
 
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
