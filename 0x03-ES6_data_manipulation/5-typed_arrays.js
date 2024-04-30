0ba443da582120449e1e3c575323470e6f2ece70
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-01 00:27:26 +0300

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
-  return int8View;
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const newBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(newBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(newBuffer);
 }
