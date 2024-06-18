60cbf891dd580e2e5c926aa0f0a4495a3ff03ad1
Collins <ochiengcollinsoduor2@gmail.com>
2024-06-18 23:07:57 +0300

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
-    process.stdout.write(`Your name is ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
