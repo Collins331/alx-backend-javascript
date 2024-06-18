463d9003ba66fcff02f04941055eee11b99cff62
Collins <ochiengcollinsoduor2@gmail.com>
2024-06-19 00:22:43 +0300

    http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const { createServer } = require('http');
+
+const hostname = '127.0.0.1';
+const port = '1245';
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
