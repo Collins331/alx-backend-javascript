a4e6aebb06f5bf7343d455872d8cf7655a7e78f2
Collins <ochiengcollinsoduor2@gmail.com>
2024-06-18 22:51:07 +0300

    update files

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 process.stdin.setEncoding('utf-8');
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on('readable', function() {
-    const name = process.stdin.read();
-    if (name) {
-        process.stdout.write(`Your name is ${name}`);
-    }
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write("This important software is now closing\n");
+  process.stdout.write('This important software is now closing\n');
 });
