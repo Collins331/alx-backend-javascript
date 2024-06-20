a490bd2b652f9b565c03ab31c0ead39a507df034
Collins <ochiengcollinsoduor2@gmail.com>
2024-06-21 00:52:44 +0300

    sinon js

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+const Utils = {
+  calculateNumber: function(type, a, b) {
+    switch(type) {
+      case 'SUM':
+        return Math.round(a) + Math.round(b);
+      case 'SUBTRACT':
+        return Math.round(a) - Math.round(b);
+      case 'DIVIDE':
+        if(Math.round(b) === 0) {
+            return "Error";
+        }
+        return Math.round(a) / Math.round(b);
+    }
+  } 
+}
+
+module.exports = Utils;
