4de1fac5336e673c34f9d322d3292cd235817703
Collins <ochiengcollinsoduor2@gmail.com>
2024-06-20 22:56:36 +0300

    update calculate number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,35 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function(){
-  it('should return 4 when input is 1, 3', function(){
-    assert.equal(calculateNumber(1, 3), 4);
+describe("Tests calculateNumber function:", function() {
+    describe("Calculates two integers:", function() {
+        it('Returns 4.', function() {
+            assert.strictEqual(calculateNumber(1, 3), 4);
+        });
+    });
+    describe("Calculates one float & one integer:", function() {
+        it('Returns 5.', function() {
+            assert.strictEqual(calculateNumber(1, 3.7), 5);
+        });
+    });
+    describe("Calculates one float & one integer (round down):", function() {
+        it('Returns 4.', function() {
+            assert.strictEqual(calculateNumber(1, 3.3), 4);
+        });
+    });
+    describe("Calculates one float & one int reversed:", function () {
+    it('Returns 5.', function () {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
   });
-  it('should return 5 when input is 1, 3.7', function(){
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it('should return 5 when input is 1.2, 3.7', function(){
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return 6 when input is 1.5, 3.7', function(){
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-})
+    describe("Calculates two floats:", function() {
+        it('Returns 5.', function() {
+            assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        });
+    });
+    describe("Calculates two floats w/borderline:", function() {
+        it('Returns 6.', function() {
+            assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        });
+    });
+});
